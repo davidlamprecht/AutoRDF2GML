729b5c53877b974c6741e7315bd71bdbc7bcd81b
littleflow3r <36025078+littleflow3r@users.noreply.github.com>
2024-11-26 10:13:31 +0900

    Update cb.py

--- library/autordf2gml/cb.py
+++ library/autordf2gml/cb.py
@@ -202,9 +202,11 @@ def content_feature(config):
     print (f'## NLD column: {pivoted_df_nld}')
 
     text_columns = []
-    for col in nodes_data_pivoted_df["pivoted_df_work"].columns:
-        sample_values = nodes_data_pivoted_df["pivoted_df_work"][col].head(1000).dropna()
-
+    # for col in nodes_data_pivoted_df["pivoted_df_work"].columns:
+    #     sample_values = nodes_data_pivoted_df["pivoted_df_work"][col].head(1000).dropna()
+    for col in nodes_data_pivoted_df[pivoted_df_nld].columns:
+        sample_values = nodes_data_pivoted_df[pivoted_df_nld][col].head(1000).dropna()
+        
         unique_strings = sample_values.nunique()
         
         avg_spaces = sample_values.apply(lambda x: str(x).count(' ')).mean()
